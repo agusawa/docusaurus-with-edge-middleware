d4fc65d16344b7f7c679105b00023a5d568fe926
Agus Stiawan <agus.stiawan2211@gmail.com>
2024-07-05 11:59:44 +0700

    chore: Comment out unused code in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { jwtVerify } from "jose";
+// import { jwtVerify } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
@@ -8,38 +8,49 @@ export const config = {
 }
 
 export default async function middleware(request: NextRequest) {
-  const token = request.cookies.get(USER_TOKEN)?.value;
-
-  try {
-    const isVerified = await jwtVerify(
-      token,
-      new TextEncoder().encode(JWT_SECRET)
-    );
-    if (isVerified) {
-      return NextResponse.next();
-    }
-  } catch (err) {}
-
   const paramToken = request.nextUrl.searchParams.get('token');
 
   if (!paramToken) {
     return NextResponse.redirect(`https://app.carops.net/login?redirectTo=${request.nextUrl}`);
   }
 
-  try {
-    const isVerified = await jwtVerify(
-      token,
-      new TextEncoder().encode(JWT_SECRET)
-    );
-    if (isVerified) {
-      request.cookies.set(USER_TOKEN, paramToken);
-      request.nextUrl.searchParams.delete('token');
+  if (paramToken === '123') {
+    return NextResponse.next();
+  }
 
-      return NextResponse.next();
-    }
-  } catch (err) {}
 
-  return NextResponse.redirect(`https://app.carops.net/login?redirectTo=${request.nextUrl}`);
+  // const token = request.cookies.get(USER_TOKEN)?.value;
+
+  // try {
+  //   const isVerified = await jwtVerify(
+  //     token,
+  //     new TextEncoder().encode(JWT_SECRET)
+  //   );
+  //   if (isVerified) {
+  //     return NextResponse.next();
+  //   }
+  // } catch (err) {}
+
+  // const paramToken = request.nextUrl.searchParams.get('token');
+
+  // if (!paramToken) {
+  //   return NextResponse.redirect(`https://app.carops.net/login?redirectTo=${request.nextUrl}`);
+  // }
+
+  // try {
+  //   const isVerified = await jwtVerify(
+  //     token,
+  //     new TextEncoder().encode(JWT_SECRET)
+  //   );
+  //   if (isVerified) {
+  //     request.cookies.set(USER_TOKEN, paramToken);
+  //     request.nextUrl.searchParams.delete('token');
+
+  //     return NextResponse.next();
+  //   }
+  // } catch (err) {}
+
+  // return NextResponse.redirect(`https://app.carops.net/login?redirectTo=${request.nextUrl}`);
 }
 
-const USER_TOKEN = 'user-token';
+// const USER_TOKEN = 'user-token';
